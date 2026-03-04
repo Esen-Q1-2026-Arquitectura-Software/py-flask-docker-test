a0f889dacb9cac74e2d422c2cb2def44d73f8e09
Balbino Aylagas <38702996+baylagas@users.noreply.github.com>
2026-03-04 07:04:44 -0600

    add flask app

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from py-flask-docker-test!")
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        a = float(request.form["a"])
+        b = float(request.form["b"])
+        result = a + b
+        return redirect(url_for("result", action="Addition", a=a, b=b, result=result))
+    return render_template("add.html")
+
+
+@app.route("/multiply", methods=["GET", "POST"])
+def multiply():
+    if request.method == "POST":
+        a = float(request.form["a"])
+        b = float(request.form["b"])
+        result = a * b
+        return redirect(url_for("result", action="Multiplication", a=a, b=b, result=result))
+    return render_template("multiply.html")
+
+
+@app.route("/result")
+def result():
+    action = request.args.get("action")
+    a = request.args.get("a")
+    b = request.args.get("b")
+    result = request.args.get("result")
+    return render_template("result.html", action=action, a=a, b=b, result=result)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
